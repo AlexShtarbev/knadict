6b8c4bdd03d855e6d0aa0762fb430f08d430f9a3
AlexShtarbev <alex.shtarbev@gmail.com>
2015-12-06 20:16:03 +0200

    added separation of subdirectory file creation

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -13,6 +13,9 @@ import java.util.Hashtable;
 public class Controller {
 	private Hashtable<String, String> _dictionatyHashtable = new Hashtable<>();
 	private static Controller _controller;
+	private static final String DIRECTORY_NAME = "KNA Dictionary";
+	private static final String FILE_NAME_KEY = "kna_";
+	private static final String EXTENSION = ".txt";
 	
 	private Controller() {};
 	
@@ -24,11 +27,11 @@ public class Controller {
 		return _controller;
 	}
 	
-	public void createDictionaryWord(String word, String translation) throws IOException {
+	public void createDictionaryWord(String word, String translation) throws IOException, FailedToCreateDictionaryDirectoryExceptions {
 		// FIXME create alert dialog
 		if(word == null || translation == null) return;
 		
-		String filePathName = provideSubDictionaryPath(word.charAt(0));
+		String filePathName = provideSubDictionaryPath(word.toLowerCase().charAt(0));
 		
 		// Get the subdictionary where the word has to be saved.
 		SubDictionary sd = new SubDictionary();
@@ -53,21 +56,46 @@ public class Controller {
 	 * Provides the path of the file where the subdictionary is saved as a JSON string.
 	 * @param firstLetter
 	 * @return
+	 * @throws FailedToCreateDictionaryDirectoryExceptions 
+	 * @throws IOException 
 	 */
-	private String provideSubDictionaryPath(char firstLetter) {
-		ensureValidPath();
-		// TODO - Kolyo - create a string of the full file path and return it (you can delete the current return statement below).
-		return "D:\\TU_3ti_Kurs\\PE\\Kursova\\kna_dictionary\\test_files\\test_dict.txt";
+	private String provideSubDictionaryPath(char firstLetter) throws FailedToCreateDictionaryDirectoryExceptions, IOException {
+		ensureSubDictionaryPathExists(firstLetter);
+		return createSubDictionaryPathFromFirsLetterOfWord(firstLetter);
 	}
 	
 	/**
 	 * If the file does not exist - it is created.
+	 * @throws FailedToCreateDictionaryDirectoryExceptions 
+	 * @throws IOException 
 	 */
-	public void ensureValidPath() {
-		// TODO - Kolyo
+	public void ensureSubDictionaryPathExists(char firstLetter) throws FailedToCreateDictionaryDirectoryExceptions, IOException {
+		ensureDirectoryExists();
+		File path = new File(createSubDictionaryPathFromFirsLetterOfWord(firstLetter));
+		if(!path.exists()) {
+			path.createNewFile();
+		}
 	}
 	
-	public String provideUserDesktopPath() {
+	private String createSubDictionaryPathFromFirsLetterOfWord(char firstLetter) throws FailedToCreateDictionaryDirectoryExceptions {
+		return provideDictionaryDirectoryPath() + "\\" + FILE_NAME_KEY + (int)firstLetter + EXTENSION;
+	}
+	
+	private void ensureDirectoryExists() throws FailedToCreateDictionaryDirectoryExceptions {
+		File directory = new File(provideDictionaryDirectoryPath());
+		if(!directory.exists()) {
+			if(!directory.mkdirs()) {
+				throw new FailedToCreateDictionaryDirectoryExceptions();
+			}
+		}
+	}
+	
+	public String provideDictionaryDirectoryPath() {
+		String directory = provideUserDesktopPath() + "\\" + DIRECTORY_NAME;
+		return directory;
+	}
+	
+	private String provideUserDesktopPath() {
 		File desktop = new File(System.getProperty("user.home"), "Desktop");
 		return desktop.getAbsolutePath();
 	}
@@ -93,6 +121,37 @@ public class Controller {
 		if(subDict == null) return;
 		for(DictionaryWord dw : subDict) {
 			_dictionatyHashtable.put(dw.getWord(), dw.getTranslation());
+		}
+	}
+	
+	public String search(String word) throws FailedToCreateDictionaryDirectoryExceptions, IOException {
+		String filePathName = provideSubDictionaryPath(word.toLowerCase().charAt(0));
+		
+		// Get the subdictionary where the word has to be saved.
+		SubDictionary sd = new SubDictionary();
+		sd.parseAll(readDictionaryFromFile(filePathName));
+		
+		// Cache all the words from the dictionary into the Global dictionary.
+		cacheWords(sd);		
+		if(_dictionatyHashtable.containsKey(word)) {
+			return _dictionatyHashtable.get(word);
+		}
+		
+		return null;
+	}
+	
+	public static class FailedToCreateDictionaryDirectoryExceptions extends Exception {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public FailedToCreateDictionaryDirectoryExceptions() {
+			super();
+		}
+		
+		public FailedToCreateDictionaryDirectoryExceptions(String message) {
+			super(message);
 		}
 	}
 }
--- src/main/Test.java
+++ src/main/Test.java
@@ -2,6 +2,8 @@ package main;
 
 import java.io.IOException;
 import java.util.Scanner;
+
+import main.Controller.FailedToCreateDictionaryDirectoryExceptions;
 /**
  * Created by Alex on 11/27/2015.
  */
@@ -11,12 +13,24 @@ public class Test {
 		Scanner sc = null;
 		try {
 			sc = new Scanner(System.in);
-			System.out.println("Please, input a word and translation in the format \"<word>,<translation>\"");
+			System.out.println("Please, input a word and translation in the format \"<cmd>,<word>,<translation>\"");
 			while(sc.hasNext()) {
-				System.out.println("Please, input a word and translation in the format \"<word>,<translation>\"");
+				System.out.println("Please, input a word and translation in the format \"<cmd>,<word>,<translation>\"");
 				String str = sc.nextLine();
-				String[] wordAndTranslation = str.split(",");
-				Controller.instance().createDictionaryWord(wordAndTranslation[0], wordAndTranslation[1]);
+				String[] input = str.split(",");
+				if(input[0].equals("p")) {
+					try {
+						Controller.instance().createDictionaryWord(input[1], input[2]);
+					} catch (FailedToCreateDictionaryDirectoryExceptions e) {
+						e.printStackTrace();
+					}
+				} else if(input[0].equals("s")) {
+					try {
+						System.out.println(Controller.instance().search(input[1]));
+					} catch (FailedToCreateDictionaryDirectoryExceptions e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
